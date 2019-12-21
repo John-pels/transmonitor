80429039fd097deaf9e1eff6e7add7f663e160c7
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-12-21 17:54:02 +0100

    Mounting Component
    I mount the sideBar and topChart components to the body container

--- src/component/container/index.jsx
+++ src/component/container/index.jsx
@@ -1,23 +1,41 @@
 import React from "react";
+import SideBarMenu from "../sideBarMenu";
+import Button from "../button";
+import TransactionBox from "../transaction";
 import "./container.styles.scss";
 
 const MainContainer = () => {
   return (
     <section className="container">
-      <div className="containerBoxes  box1"></div>
+      <div className="containerBoxes  box1">
+        <Button />
+        <SideBarMenu />
+      </div>
       <div className="containerBoxes box2">
         <div className="box2__topChart">
           <div className="topChart--items">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            <TransactionBox
+              transaction="Daily Transaction Volume"
+              figure="2,342"
+            />
           </div>
           <div className="topChart--items">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.?
+            <TransactionBox
+              transaction="Daily Transaction Value"
+              figure="₦4,000,000"
+            />
           </div>
           <div className="topChart--items">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. ?
+            <TransactionBox
+              transaction="Total Transaction Volume"
+              figure="452,000"
+            />
           </div>
           <div className="topChart--items">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.{" "}
+            <TransactionBox
+              transaction="Daily Transaction Value"
+              figure="₦4,000,000"
+            />
           </div>
         </div>
         <div className="chart">
@@ -29,12 +47,14 @@ const MainContainer = () => {
           </div>
           <div className="chart__items right-record">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo minus
-            
           </div>
         </div>
         <h2>Payments</h2>
-        <div className='table-data'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, error consequuntur. Nobis unde rem maxime soluta accusamus cupiditate, assumenda, magnam quae quibusdam sapiente minima quam earum voluptas pariatur modi in.
+        <div className="table-data">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+          error consequuntur. Nobis unde rem maxime soluta accusamus cupiditate,
+          assumenda, magnam quae quibusdam sapiente minima quam earum voluptas
+          pariatur modi in.
         </div>
       </div>
     </section>
