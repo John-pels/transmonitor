9f238c49307874d5e4a658cfa1e423e1f12fccc3
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-12-21 12:39:15 +0100

    Main container
    I create main container for all the components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
 import Header from "./component/header";
 import MainContainer from "./component/container";
-import TrackRecord from "./component/trackRecord";
 function App() {
   return (
     <div className="App">
       <Header />
       <MainContainer />
-      <TrackRecord
-        heading="Payments"
-        textOne="Un-reconcilled Payments"
-        textTwo="reconcilled Payments"
-        textThree="Total payments"
-      />
     </div>
   );
 }
--- src/component/container/index.jsx
+++ src/component/container/index.jsx
@@ -1,14 +1,41 @@
 import React from "react";
-import classes from "./container.module.scss";
+import "./container.styles.scss";
 
 const MainContainer = () => {
   return (
-    <section className={classes.container}>
-      <div className={classes.containerBoxes}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum earum, laudantium ducimus hic modi atque fugit fuga id, maiores alias nobis eos? Similique natus, perferendis officiis nulla explicabo nesciunt harum!
-      </div>
-      <div className={classes.containerBoxes}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio quasi unde similique maiores consequuntur corporis quos molestiae voluptas suscipit facere omnis veniam, itaque alias veritatis, quam consectetur corrupti iusto eius.
+    <section className="container">
+      <div className="containerBoxes  box1"></div>
+      <div className="containerBoxes box2">
+        <div className="box2__topChart">
+          <div className="topChart--items">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </div>
+          <div className="topChart--items">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.?
+          </div>
+          <div className="topChart--items">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. ?
+          </div>
+          <div className="topChart--items">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.{" "}
+          </div>
+        </div>
+        <div className="chart">
+          <div className="chart__items chart--area">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo minus
+            eligendi dignissimos. Facilis officia dolores ut, eos esse, voluptas
+            repellat ab tenetur eaque ad maxime necessitatibus quam, expedita
+            quisquam commodi?
+          </div>
+          <div className="chart__items right-record">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo minus
+            
+          </div>
+        </div>
+        <h2>Payments</h2>
+        <div className='table-data'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, error consequuntur. Nobis unde rem maxime soluta accusamus cupiditate, assumenda, magnam quae quibusdam sapiente minima quam earum voluptas pariatur modi in.
+        </div>
       </div>
     </section>
   );
